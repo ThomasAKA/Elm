f24d415126576766c188df50c1f3e688d898e1a9
ThomasAKA <akagbothomas@gmail.com>
2022-04-11 20:37:20 +0000

    first commit

---
+++ main.elm
@@ -0,0 +1,9 @@
+import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (..)
+import Browser
+import Html.Events exposing (onClick)
+
+
+main = 
+    text "Hello, you stylish developer"
+
